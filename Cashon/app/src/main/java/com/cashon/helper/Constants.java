0dff78fb49db1f8caedd231dfd971a7b87e57c3d
Rohit Garg <rt.garg1991@gmail.com>
2015-06-06 18:55:46 +0530

    Adding Helper Classes Constants and Logger

---
+++ Cashon/app/src/main/java/com/cashon/helper/Constants.java
@@ -0,0 +1,21 @@
+package com.cashon.helper;
+
+/**
+ * Created by Rohit on 6/6/2015.
+ */
+public class Constants {
+    /**
+     * Version of the App
+     */
+    public static final int VERSION = 1;
+
+    /**
+     * TAG for the Logs to be printed
+     */
+    public static final String TAG = "CashOn";
+
+    /**
+     * To check Whether secure logging is on or not
+     */
+    public static final boolean DEBUG = true;
+}
